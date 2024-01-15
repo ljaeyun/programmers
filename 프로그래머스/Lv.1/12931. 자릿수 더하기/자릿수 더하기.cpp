fb2cadfffcd75277e5d93935d16e5c06175a07e4
ljaeyun <jae9864@naver.com>
2024-01-15 10:50:38 +0900

    [level Lv.1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 3.77 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+
+    string num = to_string(n);
+    
+    for(int i = 0 ; i < num.size(); i++)
+    {
+        answer+= (num[i]-'0');
+    }
+
+    return answer;
+}
