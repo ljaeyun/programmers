f069f23084860d2e4b7b982f0b6ce1f2f4cdaf13
ljaeyun <jae9864@naver.com>
2024-03-21 15:56:16 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.24 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -0,0 +1,61 @@
+#include<vector>
+#include<queue>
+#include <cstring>
+using namespace std;
+
+int solution(vector<vector<int> > maps)
+{
+    int answer = 0;
+   
+    int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+
+// 거리표시
+int check[101][101];
+
+int visited[101][101];
+
+    memset(check, 0, sizeof(check));
+    memset(visited, 0, sizeof(visited));
+
+// 시작
+visited[0][0] = 1;
+queue<pair<int, int>>q;
+q.push(make_pair(0, 0));
+
+while (!q.empty())
+{
+    int x = q.front().first;
+    int y = q.front().second;
+
+    q.pop();
+
+    for (int i = 0; i < 4; i++)
+    {
+        int next_x = x + dx[i];
+        int next_y = y + dy[i];
+
+        if (0 <= next_x && next_x < maps.size() && 0 <= next_y && next_y < maps[0].size())
+        {
+            if (maps[next_x][next_y] == 1 && visited[next_x][next_y] == 0)
+            {
+                check[next_x][next_y] = check[x][y] + 1;
+                visited[next_x][next_y] = 1;
+                q.push(make_pair(next_x, next_y));
+            }
+        }
+    }
+}
+    if(check[maps.size()-1][maps[0].size()-1] == 0)
+    {
+        answer = -1;
+    }
+    else
+    {
+        answer = check[maps.size()-1][maps[0].size()-1]+1;
+    }
+    
+    
+   
+    return answer;
+}
